0625a58eeeedf650a9ec01e0929b12e951e9536d
Juan Estrella <juan.estrella137@gmail.com>
2020-03-16 17:35:25 +0100

    Add Experience Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_160935) do
+ActiveRecord::Schema.define(version: 2020_03_16_162821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.string "date_choices"
+    t.string "photo"
+    t.integer "max_participants"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_03_16_160935) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "experiences", "users"
 end
