ba02131eaa4984436b2f256fabf5e235d23e2468
Robert King <robert.o.king@hotmail.co.uk>
2020-03-17 11:15:32 +0100

    Generated experiences controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'experiences/index'
+  get 'experiences/show'
+  get 'experiences/new'
+  get 'experiences/create'
+  get 'pages/home'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
