417a2b30aea33c9380cb0be750dae033dfd02f30
Robert King <robert.o.king@hotmail.co.uk>
2020-03-17 11:54:07 +0100

    Basic experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,13 +1,28 @@
 class ExperiencesController < ApplicationController
   def index
-  end
-
-  def show
+    @experiences = Experience.all
   end
 
   def new
+    @experience = Experience.new
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    if @experience.save
+      redirect_to experience_path(@experience)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @experience = experience.find(params[:id])
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:name, :description, :location, :date_choices, :photo, :max_participants, :price)
   end
 end
