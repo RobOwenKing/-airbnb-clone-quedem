dc52f83ec6908125ef95256c083069ca1e02e994
Robert King <robert.o.king@hotmail.co.uk>
2020-03-17 11:23:45 +0100

    Added experiences routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'experiences/index'
-  get 'experiences/show'
-  get 'experiences/new'
-  get 'experiences/create'
-  get 'pages/home'
   devise_for :users
   root to: 'pages#home'
+  resources :experiences, only: [ :index, :show, :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
