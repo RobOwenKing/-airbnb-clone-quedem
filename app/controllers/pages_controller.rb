df559386d868f613c4a465d57f02bfec1f35002e
Ry Ellingson <rycellingson@gmail.com>
2020-03-18 14:13:49 +0100

    got the carousel working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @photos = Experience.select(:photo)
+    @experiences = Experience.all.select {|exp| exp.photo.attached?}.sample(6)
   end
 end
