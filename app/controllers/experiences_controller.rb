54bf29a9a070bc36074dec5ce265982eaf836b0b
Ry Ellingson <rycellingson@gmail.com>
2020-03-19 16:32:31 +0100

    search by location done

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,44 +1,48 @@
 class ExperiencesController < ApplicationController
   def index
+
     @experiences = policy_scope(Experience)
+    # if params[:location_query] && params[:location_query] != ""
+    #   @experiences = Experience.geocoded.near(params[:location_query], 50) #returns experiences with coordinates
+    # else
+    #   @experiences = Experience.geocoded
+    # end
 
-    @experiences = Experience.geocoded #returns experiences with coordinates
-
-    @markers = @experiences.map do |experience|
-      {
-        lat: experience.latitude,
-        lng: experience.longitude
-      }
-    end
+  @markers = @experiences.map do |experience|
+    {
+      lat: experience.latitude,
+      lng: experience.longitude
+    }
   end
+end
 
-  def new
-    @experience = Experience.new
-    authorize @experience
+def new
+  @experience = Experience.new
+  authorize @experience
+end
+
+def create
+  @experience = Experience.new(experience_params)
+  if authorize @experience
+    @experience.user = current_user
+  else
+    render :home
   end
-
-  def create
-    @experience = Experience.new(experience_params)
-    if authorize @experience
-      @experience.user = current_user
-    else
-      render :home
-    end
-    if @experience.save
-      redirect_to experience_path(@experience)
-    else
-      render :new
-    end
+  if @experience.save
+    redirect_to experience_path(@experience)
+  else
+    render :new
   end
+end
 
-  def show
-    @experience = Experience.find(params[:id])
-    authorize @experience
-  end
+def show
+  @experience = Experience.find(params[:id])
+  authorize @experience
+end
 
-  private
+private
 
-  def experience_params
-    params.require(:experience).permit(:name, :description, :location, :date_choices, :photo, :max_participants, :price)
-  end
+def experience_params
+  params.require(:experience).permit(:name, :description, :location, :date_choices, :photo, :max_participants, :price)
+end
 end
