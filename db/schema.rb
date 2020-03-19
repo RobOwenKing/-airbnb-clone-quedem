4255e5a5ebebdcc16d0a95f821afcc4a52b26790
Ry Ellingson <rycellingson@gmail.com>
2020-03-19 12:30:43 +0100

    created migration file to add lat and long columns to experience model and migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_101132) do
+ActiveRecord::Schema.define(version: 2020_03_19_112708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2020_03_18_101132) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
