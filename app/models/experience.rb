f3c86ec62bc3e4a2eb624a660182971eb333cbdd
Juan Estrella <juan.estrella137@gmail.com>
2020-03-20 16:21:43 +0100

    added necessary validations

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -4,9 +4,12 @@ class Experience < ApplicationRecord
   has_many :reviews, through: :bookings
   has_one_attached :photo
   validates :name, presence: true
+  validates :description, presence: true
   validates :location, presence: true
   validates :date_choices, presence: true
+  validates :max_participants, presence: true, numericality: true
   validates :photo, presence: true
+  validates :price, presence: true, numericality: true
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :title, presence: true
   validates :content, presence: true
-  validates :stars, presence: true
+  validates :stars, presence: true, numericality: true
 end
